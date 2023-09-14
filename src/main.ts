30ff9b802383496802e95566090ed88bbba2541e
Snakehead181 <67911084+Snakehead181@users.noreply.github.com>
2023-09-14 10:08:30 +0100

    Updated api connection details

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.use(
-  //   helmet({
-  //     contentSecurityPolicy: false,
-  //     referrerPolicy: {
-  //       policy: ['strict-origin-when-cross-origin'],
-  //     },
-  //   }),
-  // );
-  app.enableCors({ origin: true, credentials: true });
-  await app.listen(3000);
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+      referrerPolicy: {
+        policy: ['strict-origin-when-cross-origin'],
+      },
+    }),
+  );
+  app.enableCors({ origin: true });
+  await app.listen(parseInt(process.env.PORT) || 8080);
 }
 bootstrap();
